dfe580acdac2ab7a332fef3401c2ccb0227aa94d
Antonio Xavier <antonio.75bi@gmail.com>
2023-03-01 19:55:44 +0200

    Update MainLayout.vue
    #Adição de links uteis como whatsapp, instagram.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+        <strong>CHOUPAL AUTO ACESSORIOS</strong>
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>Logotipo {{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -28,7 +28,7 @@
         <q-item-label
           header
         >
-          Essential Links
+          <strong>Links Uteis</strong>
         </q-item-label>
 
         <EssentialLink
@@ -51,46 +51,22 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: 'Whatsapp',
+    caption: '+258844097201',
+    icon: 'history ',
+    link: 'https://wa.me/258844097201'
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
+    title: 'Instagram',
+    caption: '@choupal.auto.acessorios',
     icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    link: 'https://instagram.com/choupal.auto.acessorios/'
   },
   {
     title: 'Facebook',
-    caption: '@QuasarFramework',
+    caption: 'choupal.auto.acessorios',
     icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    link: 'https://facebook.com'
   }
 ]
 
