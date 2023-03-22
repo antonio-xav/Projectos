c6254e8a50aa4fbf8c4c0be42e12b10dd3d0609b
Antonio Xavier <97049491+7firestorm@users.noreply.github.com>
2023-03-22 12:32:48 +0200

    Update MainLayout.vue
    Adição de metodos de abertura de links uteis.
    Personalização do q-fab-action

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -14,7 +14,7 @@
         <q-toolbar-title>
         <!--     <img
       alt="Choupal_logo"
-      src="~assets/Choupal_logo_png.png"
+      src="~assets/Choupal_logo_01.png"
       alt="Choupal_logo"
       src=""
       style=" "
@@ -27,11 +27,12 @@
 
         </q-toolbar-title>
 
-        <div>Logotipo {{ $q.version }}</div>
+        <div>Logotipo</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer
+      v-if="$q.platform.is.desktop"
       v-model="leftDrawerOpen"
       show-if-above
       bordered
@@ -53,6 +54,18 @@
 
     <q-page-container>
       <router-view />
+      <q-page-sticky position="bottom-right" :offset="[18,18]">
+            <q-fab
+              v-if="$q.platform.is.mobile"
+              icon="add"
+              direction="up"
+              color="primary"
+            >
+              <q-fab-action @click="abrirEmail" color="primary" icon="bi-envelope-check" />
+              <q-fab-action @click="abrirInstagram" color="primary" icon="bi-instagram" />
+              <q-fab-action @click="abrirChat" type="submit" color="primary" icon="bi-whatsapp"  />
+          </q-fab>
+          </q-page-sticky>
     </q-page-container>
   </q-layout>
 
@@ -62,8 +75,10 @@
 import { defineComponent, ref } from 'vue'
 import EssentialLink from 'components/EssentialLink.vue'
 import { biWhatsapp, biInstagram, biEnvelopeCheck } from '@quasar/extras/bootstrap-icons'
+import { useQuasar } from 'quasar'
 
 const linksList = [
+
   {
     title: 'Whatsapp',
     caption: '+258844097201',
@@ -87,12 +102,27 @@ const linksList = [
 
 export default defineComponent({
   name: 'MainLayout',
-
   components: {
     EssentialLink
   },
+  methods: {
+    abrirChat: function () {
+      const url = 'https://wa.me/258844097201'
+      window.open(url, '_blank')
+    },
+    abrirEmail: function () {
+      const url = 'mailto:choupalautoacessorios@gmail.com'
+      window.open(url, '_blank')
+    },
+    abrirInstagram: function () {
+      const url = 'https://instagram.com/choupal.auto.acessorios'
+      window.open(url, '_blank')
+    }
+
+  },
 
   setup () {
+    const $q = useQuasar()
     const leftDrawerOpen = ref(false)
 
     return {
@@ -101,6 +131,7 @@ export default defineComponent({
       biWhatsapp,
       biInstagram,
       biEnvelopeCheck,
+      $q,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
       }
